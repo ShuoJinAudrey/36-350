bc6a76ca40a599897f6ce2dae674aa5acd6e4ed5
ShuoJinAudrey <60403619+ShuoJinAudrey@users.noreply.github.com>
2021-08-05 21:31:56 +0800

    Update hello_aliens.R

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,8 +1,9 @@
-Talking to intelligent extraterrestrials from an newly discorvered
-habitable-zone, Earth-like exoplanets would not be a good idea.
-What if the extraterrestrials possess more powerful technologies and
-weapons than us and they intend to conquer the Earth? What if they are
-hypocritical and we have no idea? Limited communications can hinder
-our predictions and understandings of intelligent extraterrestrials and
-their planet, and it can lead to a catastrophe.
-Maybe, we just need to stay silent.
+cat("Talking to intelligent extraterrestrials from an newly discorvered
+habitable-zone, Earth-like exoplanets would not be a good idea.",
+"\nWhat if the extraterrestrials possess more powerful technologies and
+weapons than us and they intend to conquer the Earth?", "What if they are
+hypocritical and we have no idea? ",
+"Limited communications can hinder
+our predictions and understandings of intelligent extraterrestrials", "and
+their planet, and it can lead to a catastrophe.",
+"Maybe, we just need to stay silent.")
